7d9bfe7eba2707a307231eccd7c8d41abb6deb47
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2020-09-30 15:41:15 +0200

    update --unequal to store_true (improvement)

--- src/options.py
+++ src/options.py
@@ -56,9 +56,8 @@ def args_parser():
                         of optimizer")
     parser.add_argument('--iid', type=int, default=1,
                         help="Default set to IID. Set to 0 for non-IID.")
-    parser.add_argument('--unequal', type=int, default=0,
-                        help="whether to use unequal data splits for  \
-                        non-i.i.d setting (use 0 for equal splits)")
+    parser.add_argument('--unequal', action='store_true', default=False,
+                        help="whether to use unequal data splits for non-i.i.d setting")
     parser.add_argument('--stopping_rounds', type=int, default=10,
                         help="rounds of early stopping")
     parser.add_argument('--verbose', type=int, default=1, help="verbose")
